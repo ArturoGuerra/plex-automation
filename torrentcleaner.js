d2790ceba583d575ec4916019a148ba1512ffa7c
Arturo Guerra <arturoguerra360@gmail.com>
2018-03-27 00:17:29 -0600

    Fixed torrentcleaner shit

--- torrentcleaner.js
+++ torrentcleaner.js
@@ -20,30 +20,27 @@ async function checkFile(file) {
 
 async function getAllActiveTorrents(){
     transmission.get(async function(err, result){
-    console.log('\n--------------------------')
-    console.log("Starting TorrentCleaner...")
-    if (err){
-        console.log(err);
-    }
-    else {
-        for (var i=0; i < result.torrents.length; i++){
-            let del = false
-            let files = result.torrents[i].files
-            let fpath = result.torrents[i].downloadDir
-            for (let i =0; i < files.length; i++) {
-                del = await checkFile(fpath + '/' + files[i].name);
-                if (del) {break;};
-
-            }
-            if (del) {
-                console.log("Deleting " + result.torrents[i].name);
-                deleteTorrent(result.torrents[i].id);
-            } else {
-                console.log("Torrent " + result.torrents[i].name + " is still in use");
+        console.log('\n--------------------------')
+        console.log("Starting TorrentCleaner...")
+        if (err) console.log(err);
+        else {
+            for (var i=0; i < result.torrents.length; i++){
+                let del = false
+                let files = result.torrents[i].files
+                let fpath = result.torrents[i].downloadDir
+                for (let i =0; i < files.length; i++) {
+                    del = await checkFile(fpath + '/' + files[i].name);
+                    if (del) {break;}; 
+                }
+                if (del) {
+                    console.log("Deleting " + result.torrents[i].name);
+                    deleteTorrent(result.torrents[i].id);
+                } else {
+                    console.log("Torrent " + result.torrents[i].name + " is still in use");
+                }
             }
         }
-    }
-    console.log('--------------------------\n')
+        console.log('--------------------------\n')
     });
 }
 
